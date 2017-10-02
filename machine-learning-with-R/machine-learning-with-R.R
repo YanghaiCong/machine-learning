cffdf6f153287b11ff7a10fdc627dad5fdb798e6
Yanghai Cong <32158830+YanghaiCong@users.noreply.github.com>
2017-10-02 11:58:50 +0800

    modified the chapter 03

--- machine-learning-with-R/machine-learning-with-R.R
+++ machine-learning-with-R/machine-learning-with-R.R
@@ -1,10 +1,22 @@
 # machine learning with R
 
+setwd('/Users/yanghai/Documents/Yanghai的文件与资料/学习工作资料/课外专业学习资料/learning/R/MLwR-master/Machine Learning with R (2nd Ed.)');
+
 ## chapter1: introduction to machine learning
+
+### skipped
 
 ## chapter2: managing and understanding data
 
+### skipped
+
 ## chapter3: lazy learning - classificatin using nearest neighbors
+wbcd<-read.csv(
+    file = 'Chapter 03/wisc_bc_data.csv',
+    stringsAsFactors = FALSE)
+
+str(wbcd)
+
 
 ## chapter4: probabilistic learning - classification usin naive bayes
 
