a8ca2116a5da12bddbb18dae107a04b275082ee9
Yanghai Cong <32158830+YanghaiCong@users.noreply.github.com>
2017-10-02 11:39:33 +0800

    add content table

--- machine-learning-with-R/machine-learning-with-R.R
+++ machine-learning-with-R/machine-learning-with-R.R
@@ -1,1 +1,14 @@
 # machine learning with R
+
+## chapter1: introduction to machine learning
+
+## chapter2: managing and understanding data
+
+## chapter3: lazy learning - classificatin using nearest neighbors
+
+## chapter4: probabilistic learning - classification usin naive bayes
+
+## chapter5: divide and conquer - classsification using decision trees and rules
+
+## 
+
